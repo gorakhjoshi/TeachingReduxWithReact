a7389b3e0f9014ed9ee8fe771938e43727e4f4fa
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-25 21:08:09 +0545

    accessing store from component

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = {counter: 0}, action) {
-    if (action.type === 'increment') {
-    return {conter: state.counter + 1}
-    }
-    if (action.type === 'decrement') {
-    return {conter: state.counter - 1}
-    }
-    return state;
-}
+const counterReducer = (state = { counter: 10 }, action) => {
+  if (action.type === 'increment') {
+    return { conter: state.counter + 1 };
+  }
+  if (action.type === 'decrement') {
+    return { conter: state.counter - 1 };
+  }
+  return state;
+};
 
 const store = createStore(counterReducer);
 
