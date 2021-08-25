7dba0791823aae598e594a88e83716ee386e2ddd
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-25 20:45:58 +0545

    providing store

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'redux';
+
+const counterReducer = (state = {counter: 0}, action) {
+    if (action.type === 'increment') {
+    return {conter: state.counter + 1}
+    }
+    if (action.type === 'decrement') {
+    return {conter: state.counter - 1}
+    }
+    return state;
+}
+
+const store = createStore(counterReducer);
+
+export default store;
