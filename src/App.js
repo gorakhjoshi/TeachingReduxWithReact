4cda7353e361f81d9ceb9a8bf84c9e561b25f5f1
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-29 20:50:37 +0545

    multiple components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
 
 function App() {
   return (
     <div className='App'>
+      <Header />
+      <Auth />
       <Counter />
     </div>
   );
