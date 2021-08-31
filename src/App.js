f9273cfc7d92594605b3aaad2d8ef8b0dd57875f
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-08-31 20:53:49 +0545

    redux completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { useSelector } from 'react-redux';
 import './App.css';
-import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
   return (
     <div className='App'>
       <Header />
-      <Auth />
-      <Counter />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
     </div>
   );
 }
